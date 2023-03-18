7a584c657cabd93b0f5872b8488fb54930601788
Randall Nagy <randall.nagy@soft9000.com>
2023-03-18 10:00:37 -0400

    Wow - that was easy ...

--- GitUp.py
+++ GitUp.py
@@ -1,3 +1,5 @@
+import os
+
 from SaveIO import IoString as IoString
 
 class GitUp(IoString):
@@ -5,30 +7,75 @@ class GitUp(IoString):
     def __init__(self, git_project):
         super().__init__(git_project)
 
-    def write(astring)->bool:
-        pass
+    def write(self, a_string)->bool:
+        return super().save_out(a_string)
     
     def read(self)->str():
-        pass
+        return super().read_latest()
 
-    def sync(self)->bool:
-        pass
+    def sync(self, message)->bool:
+        state = self._git_add()
+        if state:
+            state = self._git_commit(message)
+        if state == True:
+            state = self._git_push()
+        return state
+
+    def _git_pull(self)->int:
+        return self._git_exec(['pull'])
+
+    def _git_add(self)->int:
+        return self._git_exec(['add', f"*{self.file_type}"])
+
+    def _git_commit(self, message)->bool:
+        return bool(self._git_exec(['commit', message]))
+
+    def _git_push(self)->bool:
+        return bool(self._git_exec(['push']))
+
+    def _git_exec(self, params)->int:
+        commands = 'git '
+        op = params[0]
+        commands += op
+        commands += ' '
+        if op == 'commit':
+            commands += '-m "'
+            commands += params[1]
+            commands += '"'
+        else:
+            for param in params[1:]:
+                commands += ' '
+                commands += param
+        success = False
+        pwd = os.getcwd()
+        try:
+            os.chdir(self.git_folder)
+            if os.getcwd() != self.git_folder:
+                raise Exception(f"Error: Unable to change to [{self.git_folder}]")
+            with os.popen(commands.strip()) as proc:
+                print(*proc)
+            success = True
+        except Exception as ex:
+            os.chdir(pwd)
+            print(ex)
+        return success
     
 
 if __name__ == '__main__':
-    test = GitUp('.')
-    test.remove_all_files()
-    if not test.save_out('nagy'):
+    TEST_MESSAGE = 'nagy'
+    test = GitUp(os.getcwd())
+    if not test.write(TEST_MESSAGE):
         raise Exception("Unable to save file.")
-    results = test.read_latest()
-    print(results)
-    if results != 'nagy':
+    if not test.sync("Test case 2"):
+        raise Exception("Unable to sync file.")
+    results = test.read()
+    if results != TEST_MESSAGE:
         raise Exception("Unable to read file.")
     results = test.list_all()
     if not results:
         raise Exception("Unable to list file.")
     success = False
-    if len(results) == 1:
+    if len(results):
         test.remove_all_files()
         if not test.list_all():
             print("Testing success.")
